f279089b81967772800ad52d1b298a63fe54d3fe
JasonMai233 <112644721+JasonMai233@users.noreply.github.com>
2025-03-19 19:49:08 -0400

    add demo for function 3

--- demo.py
+++ demo.py
@@ -1,8 +1,6 @@
 #same as the __main__ file rn (which can also be used for testing) but that may change in the future 
 
-from artificial_unintelligence.functions import get_random_word, random_sentence_reply
-#idk this doesn't work 
-#from artificial_unintelligence.function_3 import function_3  # Assuming function_3 is imported from this module
+from artificial_unintelligence.functions import get_random_word, random_sentence_reply, same_start_letter_generate
 
 def main():
     print("Welcome to Artificial Unintelligence Demo!")
@@ -14,9 +12,10 @@ def main():
     print("\n2. Random Sentence Reply:")
     print(f"Random sentence: {random_sentence_reply()}")
     
-    print("\n3. Function 3")
-  
-    
+    print("\n3. Random Sentence made of word start with same letter:")
+    letter = input(f"give me a random letter: ")
+    print(f"Random Sentence made of word start with " + letter + ": " + same_start_letter_generate(letter))
+
     print("\n4. Function 4")
  
 
