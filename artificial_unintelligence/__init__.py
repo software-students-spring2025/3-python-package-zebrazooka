5330d3a63954366b0353dc194834c09189176f78
dibukseid <dseid@bffbelau.org>
2025-03-19 20:40:38 -1000

    updated init

---
+++ artificial_unintelligence/__init__.py
@@ -0,0 +1,3 @@
+from .functions import get_random_word, random_sentence_reply, generate_sentence, keyboard_smash
+
+__all__ = ["get_random_word", "random_sentence_reply", "generate_sentence", "keyboard_smash"]
