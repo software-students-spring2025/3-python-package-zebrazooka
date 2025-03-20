d53e72540583f572a69a0ee4030a3ff9bb6e5588
dibukseid <dseid@bffbelau.org>
2025-03-19 20:46:46 -1000

    updated init and fixed errors

--- artificial_unintelligence/__init__.py
+++ artificial_unintelligence/__init__.py
@@ -1,3 +1,3 @@
-from .functions import get_random_word, random_sentence_reply, generate_sentence, keyboard_smash
+from .functions import get_random_word, random_sentence_reply, same_start_letter_generate, keyboard_smash
 
-__all__ = ["get_random_word", "random_sentence_reply", "generate_sentence", "keyboard_smash"]
+__all__ = ["get_random_word", "random_sentence_reply", "same_start_letter_generate", "keyboard_smash"]
